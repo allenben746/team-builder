fd05358c1259d72c4545ad05b76fb112e48cbb1c
Ben <allenben@protonmail.com>
2019-07-24 12:38:09 -0500

    Cleaned up boilerplate code - added useState

---
+++ team-builder/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import React, { useState } from 'react';
+
+import './App.css';
+
+function App() {
+  const [userData, setUserData] = useState({username: "", email: "", password: ""})
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
