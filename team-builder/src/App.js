bdfe82668ba46c3a7e31ae65ec5909ca77672a5a
Ben <allenben@protonmail.com>
2019-07-24 13:51:48 -0500

    Created basic layout of form

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import React, { useState } from 'react';
 
 import './App.css';
@@ -7,7 +6,17 @@ function App() {
   const [userData, setUserData] = useState({username: "", email: "", password: ""})
   return (
     <div className="App">
-
+      <section className="formContainer">
+        <form>
+          <legend>Sign Up</legend>
+            <label for="username">Username</label>
+              <input type="text" name="username" placeholder="username"></input>
+            <label for="email">Email</label>
+              <input type="text" name="email" placeholder="email"></input>
+            <label for="password">Password</label>
+              <input type="text" name="password" placeholder="password"></input>
+        </form>
+      </section>
     </div>
   );
 }
